a114ffdefc5602c0a1110af7ff730b3d81413259
Szymon Kaczmarek <54668032+SzymonKaczmarek@users.noreply.github.com>
2022-06-30 17:33:52 +0200

    test bilda

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,6 +1,8 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 import clsx from 'clsx'
 
+
+
 export default function Document(props) {
   let pageProps = props.__NEXT_DATA__?.props?.pageProps
 
@@ -20,16 +22,15 @@ export default function Document(props) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
         
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js" />
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
 
+     
       </Head>
       <body className="flex h-full flex-col">
         <Main />
         <NextScript />
-        <script>
+        {/* <script>
           AOS.init();
-        </script>
+        </script> */}
       </body>
     </Html>
   )
